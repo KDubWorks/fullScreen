8c277ce3ce43b5125f5d63e14b9d483111a6a30b
kenstjonweeks <kennethweeks@Kenneths-MacBook-Pro.local>
2017-09-17 23:01:09 -0700

    change

--- js/edit.js
+++ js/edit.js
@@ -14,86 +14,15 @@ for(var i = 0; i < editorSec.length; i++) {
 
 });*/
 
-function loop(mar) {
+$(".editorSec").on("swipeleft", function() {
 
-	if(mar == 0) {
+	$(this).css("margin-left", "-20vw");
 
-		$(".editorSec").on("swipeleft", function() {
+});
 
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-left", "-20vw");
-	
-			closeLoop(n);
 
-		});
+$(".editorSec").on("swiperight", function() {
 
-	}
+	$(this).css("margin-left", "0vw");
 
-}
-
-function closeLoop(num) {
-
-	if(num == 0) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-
-	} else if(n == 1) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-
-	} else if(n == 2) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-		
-	} else if(n == 3) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-		
-	} else if(n == 4) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-		
-	} else if(n == 5) {
-
-		$(edSeA[num]).on("swiperight", function() {
-
-			var n = edSeA.indexOf(this);
-			$(this).css("margin-right", "0vw");
-	
-
-		});
-		
-	}
-
-}
-
+});
