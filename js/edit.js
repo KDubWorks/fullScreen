7bd54a92c8e5256d9fc6b30c6e07fe2f7ca48d82
kenstjonweeks <kennethweeks@Kenneths-MacBook-Pro.local>
2017-09-17 22:38:30 -0700

    change

--- js/edit.js
+++ js/edit.js
@@ -1,6 +1,33 @@
+var win = $(window).width();
+
+var editorSec = $(".editorSec");
+var edSeA = new Array();
+
+for(var i = 0; i < editorSec.length; i++) {
+	edSeA[i] = editorSec[i];
+}
+
 $(".editorSec").on("swipeleft", function() {
 
+	var n = edSeA.indexOf(this);
+	alert(n);
+	var swipeMargin = editorSec[n].style.marginLeft;
 	$(this).css("margin-left", "-20vw");
+	closeSwipe(swipeMargin);
 
 });
+
+function closeSwipe(t) {
+
+	if(t == "-20vw") {
+
+		$(".editorSec").on("swiperight", function() {
+
+			$(this).css("margin-left", "0vw");
+
+		});
+
+	}
+
+}
 
