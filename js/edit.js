6cd67259d373ed0ab67504a9febeb217756bbac0
kenstjonweeks <kennethweeks@u-10-88-145-113.dhcp.unlv.edu>
2017-09-18 09:51:47 -0700

    change

--- js/edit.js
+++ js/edit.js
@@ -19,3 +19,43 @@ $(".editorSec").on("swiperight", function() {
 	$(this).css("margin-left", "0vw");
 
 });
+
+var use = document.querySelectorAll(".use");
+var useA = new Array();
+
+var learn = document.querySelectorAll(".learn");
+var learnA = new Array();
+
+for(var i = 0; i < learn.length; i++) {
+	learnA[i] = learn[i];
+	useA[i] = use[i];
+}
+
+var tools = $(".tools");
+
+for(var i = 0; i < use.length; i++) {
+
+	$(use[i]).on("tap", function() {
+
+		var n = useA.indexOf(this);
+		$("#toolsEditor").css("top", "0vh");
+
+		if(n == 0) {
+
+			$(tools[n]).css("display", "block");
+
+		}
+
+	});
+
+}
+
+for(var i = 0; i < learn.length; i++) {
+
+	$(learn[i]).on("tap", function() {
+
+		var n = learnA.indexOf(this);
+
+	});
+
+}
