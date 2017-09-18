111c9701bb5387f1d6ffa2c4005bdfe59f67e9b8
kenstjonweeks <kennethweeks@Kenneths-MacBook-Pro.local>
2017-09-17 23:02:53 -0700

    change

--- js/edit.js
+++ js/edit.js
@@ -7,13 +7,6 @@ for(var i = 0; i < editorSec.length; i++) {
 	edSeA[i] = editorSec[i];
 }
 
-/*$(".editorSec").on("swipeleft", function() {
-
-	var n = edSeA.indexOf(this);
-	$(this).css("margin-left", "-20vw");
-
-});*/
-
 $(".editorSec").on("swipeleft", function() {
 
 	$(this).css("margin-left", "-20vw");
