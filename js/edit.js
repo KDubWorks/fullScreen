85551591385c86a2c0fc91e3d42114e89950dca5
kenstjonweeks <kennethweeks@u-10-88-41-189.dhcp.unlv.edu>
2017-09-19 09:57:05 -0700

    change

--- js/edit.js
+++ js/edit.js
@@ -7,112 +7,116 @@ for(var i = 0; i < editorSec.length; i++) {
 	edSeA[i] = editorSec[i];
 }
 
-$(".editorSec").on("swipeleft", function() {
 
-	$(this).css("margin-left", "-8em");
+if(win <= 425) {
 
-});
+	$(".editorSec").on("swipeleft", function() {
 
-
-$(".editorSec").on("swiperight", function() {
-
-	$(this).css("margin-left", "0vw");
-
-});
-
-var use = document.querySelectorAll(".use");
-var useA = new Array();
-
-var learn = document.querySelectorAll(".learn");
-var learnA = new Array();
-
-for(var i = 0; i < learn.length; i++) {
-	learnA[i] = learn[i];
-	useA[i] = use[i];
-}
-
-var open = 0;
-
-var editTitle = $(".editTitle");
-var editTitleA = new Array();
-
-for(var i = 0; i < editTitle.length; i++) {
-	editTitleA[i] = editTitle[i];
-}
-
-$(".editTitle").on("tap", function() {
-
-	var n = editTitleA.indexOf(this);
-	$(editorSec[n]).css("margin-left", "-8em");
-
-});
-
-var tools = $(".tools");
-
-for(var i = 0; i < use.length; i++) {
-
-	$(use[i]).on("tap", function() {
-
-		var n = useA.indexOf(this);
-		$("#toolsEditor").css("top", "0vh");
-
-		if(n == 0) {
-
-			$(tools[n]).css("display", "block");
-
-		} else if(n == 1) {
-
-			$(tools[n]).css("display", "block");
-
-		} else if(n == 2) {
-
-			$(tools[n]).css("display", "block");
-
-		} else if(n == 3) {
-
-			$(tools[n]).css("display", "block");
-
-		} else if(n == 4) {
-
-			$(tools[n]).css("display", "block");
-
-		} else if(n == 5) {
-
-			$(tools[n]).css("display", "block");
-
-		}
+		$(this).css("margin-left", "-8em");
 
 	});
 
-}
 
-for(var i = 0; i < learn.length; i++) {
+	$(".editorSec").on("swiperight", function() {
 
-	$(learn[i]).on("tap", function() {
-
-		var n = learnA.indexOf(this);
-
+		$(this).css("margin-left", "0vw");
 
 	});
 
-}
+	var use = document.querySelectorAll(".use");
+	var useA = new Array();
 
-/*This will open the specific tools*/
-var sectools = $(".sectools");
-var sectoolsA = new Array();
+	var learn = document.querySelectorAll(".learn");
+	var learnA = new Array();
 
-for(var i = 0; i < sectools.length; i++) {
-	sectoolsA[i] = sectools[i];
-}
+	for(var i = 0; i < learn.length; i++) {
+		learnA[i] = learn[i];
+		useA[i] = use[i];
+	}
 
-for(var i = 0; i < sectools.length; i++) {
+	var open = 0;
 
-	$(sectools[i]).on("tap", function() {
+	var editTitle = $(".editTitle");
+	var editTitleA = new Array();
 
-		var n = sectoolsA.indexOf(this);
-		$(sectools[n]).css("height", "auto");
+	for(var i = 0; i < editTitle.length; i++) {
+		editTitleA[i] = editTitle[i];
+	}
+
+	$(".editTitle").on("tap", function() {
+
+		var n = editTitleA.indexOf(this);
+		$(editorSec[n]).css("margin-left", "-8em");
 
 	});
 
-}
+	var tools = $(".tools");
 
+	for(var i = 0; i < use.length; i++) {
+
+		$(use[i]).on("tap", function() {
+
+			var n = useA.indexOf(this);
+			$("#toolsEditor").css("top", "0vh");
+
+			if(n == 0) {
+
+				$(tools[n]).css("display", "block");
+
+			} else if(n == 1) {
+
+				$(tools[n]).css("display", "block");
+
+			} else if(n == 2) {
+
+				$(tools[n]).css("display", "block");
+
+			} else if(n == 3) {
+
+				$(tools[n]).css("display", "block");
+
+			} else if(n == 4) {
+
+				$(tools[n]).css("display", "block");
+
+			} else if(n == 5) {
+
+				$(tools[n]).css("display", "block");
+
+			}
+
+		});
+
+	}
+
+	for(var i = 0; i < learn.length; i++) {
+
+		$(learn[i]).on("tap", function() {
+
+			var n = learnA.indexOf(this);
+
+
+		});
+
+	}
+
+	/*This will open the specific tools*/
+	var sectools = $(".sectools");
+	var sectoolsA = new Array();
+
+	for(var i = 0; i < sectools.length; i++) {
+		sectoolsA[i] = sectools[i];
+	}
+
+	for(var i = 0; i < sectools.length; i++) {
+
+		$(sectools[i]).on("tap", function() {
+
+			var n = sectoolsA.indexOf(this);
+			$(sectools[n]).css("height", "auto");
+
+		});
+
+	}
+
+}
